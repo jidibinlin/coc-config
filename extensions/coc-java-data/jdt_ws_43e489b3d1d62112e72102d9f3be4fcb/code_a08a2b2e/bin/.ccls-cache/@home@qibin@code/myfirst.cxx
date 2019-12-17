261d8581e417066375705e429a74602423e942cb
jidibinlin <2694273649@qq.com>
2019-12-17 18:48:15 +0800

    first commit

---
+++ extensions/coc-java-data/jdt_ws_43e489b3d1d62112e72102d9f3be4fcb/code_a08a2b2e/bin/.ccls-cache/@home@qibin@code/myfirst.cxx
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+
+	int pencil;
+	std::cout << "please enter the number of pencil: ";
+	std::cin >> pencil;
+	std::cout << "I have " 
+			  << pencil 
+			  << " pencil." 
+			  << std::endl;
+	string a;
+	return 0;
+}
+
+
+
